2cb20e20238e13c8efb9b560f5de04d37467715b
Erica O'Kelly <okellyerica@gmail.com>
2024-07-01 17:29:46 -0400

    refactor: Added comment to code

--- bikeshare_copy.py
+++ bikeshare_copy.py
@@ -226,6 +226,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            #This is the print statment I added
             print('Thank you and have a good day')
             break
 
